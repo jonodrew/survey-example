28e6011efd9ae0f3876a02d53f44eaf89d37fb81
jonathan.kerr <jonathan.kerr@digital.cabinet-office.gov.uk>
2023-06-16 10:13:20 +0100

    Add validator for Cabinet Office emails
    This is a non-exhaustive and extensible approach to verifying Cabinet Office email. There is more than one domain within the CO, and this code attempts to be flexible enough to deal with all of them.
    
    There is no validation on whether the user accidentally added a space or comma or similar - they will need to try it again.

--- tests/test_forms.py
+++ tests/test_forms.py
@@ -12,17 +12,34 @@ class FeedbackFormTest(TestCase):
         email_errors = form.errors["email"]
         self.assertTrue("Enter a valid email address" in str(email_errors))
 
-    def test_correct_email(self):
+    def test_good_email_with_bad_domain(self):
         form = forms.FeedbackForm({"email": "test@example.com", "content": "some feedback"})
         form.is_valid()
-        no_email_errors = "email" not in form.errors
-        self.assertTrue(no_email_errors)
+        email_errors = form.errors["email"]
+        self.assertTrue("Please enter a Cabinet Office email address" in str(email_errors))
+
+    def test_good_email(self):
+        cases = (
+            {"case": "No 10", "address": "PM@No10.gov.uk"},
+            {"case": "GDS", "address": "CEO@digital.cabinet-office.gov.uk"},
+        )
+        for test_case in cases:
+            with self.subTest(test_case['case']):
+                form = forms.FeedbackForm({"email": test_case['address'], "content": "some feedback"})
+                form.is_valid()
+                no_email_errors = "email" not in form.errors
+                self.assertTrue(no_email_errors)
 
 
 class CabinetOfficeValidationTest(SimpleTestCase):
     def test_is_cabinet_office_email(self):
-        valid_email = "valid@example.com"
-        result = forms.is_cabinet_office_email(valid_email)
-        self.assertTrue(result)
+        cases = (
+            {"case": "No 10", "address": "PM@No10.gov.uk"},
+            {"case": "GDS", "address": "CEO@digital.cabinet-office.gov.uk"},
+        )
+        for test_case in cases:
+            with self.subTest(test_case['case']):
+                result = forms.is_cabinet_office_email(test_case['address'])
+                self.assertTrue(result)
 
 
